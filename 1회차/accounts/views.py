82ebbceaba566c46c1c3a55bf550a93dcd8d8867
pangpyo <kkp0639@gmail.com>
2022-11-25 09:14:51 +0900

    push

--- 1회차/accounts/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-# Create your views here.
-
-
-@api_view()
-def hello_world(request):
-    return Response({"message": "Hello, world!"})
