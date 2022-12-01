791d6d5e7103ede54912d814470308e2adeab72f
wxxk <dwde22@gmail.com>
2022-12-01 19:35:22 +0900

    2번째 과제

---
+++ 1201/BlogApp/users/forms.py
@@ -0,0 +1,7 @@
+from django_registration.forms import RegistrationForm
+from .models import UserModel
+
+
+class UserForm(RegistrationForm):
+    class Meta(RegistrationForm.Meta):
+        model = UserModel
