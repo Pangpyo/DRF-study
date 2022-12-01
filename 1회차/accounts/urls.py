7f1d6f049d4e440282c51e17721ba6db4b0d321f
pangpyo <kkp0639@gmail.com>
2022-12-01 22:38:16 +0900

    1201

--- 1회차/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-from rest_framework import urls
-
-urlpatterns = [
-    path("signup/", views.UserCreate.as_view()),
-    path("api-auth/", include("rest_framework.urls")),
-]
--- 1회차/accounts/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .serializers import UserSerializer
-from .models import User
-from rest_framework import generics
-
-# 회원가입
-class UserCreate(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
