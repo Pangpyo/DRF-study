932d9746f53d56648465045497bc6cf9859ff625
uRo3YA <ltg8693@gmail.com>
2022-12-03 17:46:09 +0900

    이태극  2회차 과제

--- uRo3YA/account/urls.py
+++ uRo3YA/account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from . import views
 from rest_framework import urls
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns =[
     path('signup/', views.UserCreate.as_view()),
     path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', obtain_auth_token),
  ]
--- uRo3YA/project/views.py
+++ uRo3YA/project/views.py
@@ -1,12 +1,26 @@
-from .models import Blog
-from .serializers import BlogSerializer
+from .models import Blog, Comment
+from .serializers import BlogSerializer, CommentSerializer
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsAuthorOrReadOnly
 
-# Blog의 목록, detail 보여주기, 수정하기, 삭제하기 모두 가능
+# (게시글) Blog의 목록, detail 보여주기, 수정하기, 삭제하기 모두 가능
 class BlogViewSet(viewsets.ModelViewSet):
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
    
-   	# serializer.save() 재정의
+    def perform_create(self, serializer):
+        serializer.save(user = self.request.user)
+
+# (댓글) Comment 보여주기, 수정하기, 삭제하기 모두 가능
+class CommentViewSet(viewsets.ModelViewSet):
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
     def perform_create(self, serializer):
         serializer.save(user = self.request.user)
