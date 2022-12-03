5f0bcfed267e4be4b7e2f38d3bb678f25b6af573
uRo3YA <ltg8693@gmail.com>
2022-12-03 18:10:51 +0900

    브랜치 수정

--- uRo3YA/drf/settings.py
+++ uRo3YA/drf/settings.py
@@ -11,16 +11,35 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import json
+from django.core.exceptions import ImproperlyConfigured
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+secret_file = os.path.join(BASE_DIR, "secrets.json")  # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y(hxt+r^b1x+#bx2=8c-8%k$u-6+j75bs8yg6rwrx)yk*+l)pj'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
